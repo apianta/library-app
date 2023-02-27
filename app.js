db5f13b1fdfc2bb23a9f572d9d4e8726d2a322dd
Andrew Pianta <a.pianta92@gmail.com>
2023-02-27 11:47:11 -0500

    Add Library class

--- app.js
+++ app.js
@@ -2,15 +2,39 @@
 // title
 // author
 // number of pages
+class Book {
+	constructor(
+		title = "Unknown",
+		author = "Unknown",
+		pages = "0",
+		isRead = false
+	) {
+		(this.title = title),
+			(this.author = author),
+			(this.pages = pages),
+			(this.isRead = isRead);
+	}
+}
 
 class Library {
 	constructor() {
 		this.book = [];
 	}
+
+	addBook(newBook) {
+		if (!this.isInLibrary(newBook)) {
+			this.books.push(newBook);
+		}
+	}
+	removeBook(title) {
+		this.books = this.books.filter((book) => book.title !== title);
+	}
+	getBook(title) {
+		return this.books.find((book) => book.title === title);
+	}
+	isInLibrary(newBook) {
+		return this.books.some((book) => book.title === newBook.title);
+	}
 }
 
-const library = [];
-
-function Book() {}
-
-function addBookToLibrary() {}
+const library = newLibrary();
